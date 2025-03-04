7b99f5521a9dc48cc78f2b5caf75469117b1a0ad
saurabh wanjari <141421981+wanjarisaurabh@users.noreply.github.com>
2025-03-04 16:15:52 +0000

    y

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -1,25 +1,30 @@
-const promiseOne = new Promise(function(resolve, reject){
-    //Do an async task
-    // DB calls, cryptography, network
-    setTimeout(function(){
-        console.log('Async task is compelete');
-        resolve()
-    }, 1000)
-})
+//read from the mdn
 
-promiseOne.then(function(){
-    console.log("Promise consumed");
-})
 
-new Promise(function(resolve, reject){
-    setTimeout(function(){
-        console.log("Async task 2");
-        resolve()
-    }, 1000)
+// const promiseOne = new Promise(function(resolve, reject){
+//     //Do an async task
+//     // DB calls, cryptography, network
+//     setTimeout(function(){
+//         console.log('Async task is compelete');
+//         resolve()
+//     }, 1000)
+// })
 
-}).then(function(){
-    console.log("Async 2 resolved");
-})
+// const promiseone = new Promise()
+
+// promiseOne.then(function(){
+//     console.log("Promise consumed");
+// })
+
+// new Promise(function(resolve, reject){  
+//     setTimeout(function(){
+//         console.log("Async task 2");
+//         resolve()
+//     }, 1000)
+
+// }).then(function(){
+//     console.log("Async 2 resolved");
+// })
 
 const promiseThree = new Promise(function(resolve, reject){
     setTimeout(function(){
